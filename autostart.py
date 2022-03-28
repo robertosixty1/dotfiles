3b0a3a30691868a9e48ab0b83ff89bb54fd49769
robertohermenegildodias <roberto011herminegildo@gmail.com>
2022-03-28 13:32:39 -0300

    Move the function to change wallpaper randomly to autostart.py

--- autostart.py
+++ autostart.py
@@ -1,1 +1,17 @@
 #!/bin/env python3
+
+import os
+import subprocess
+import random
+
+wallpapers = []
+
+def set_random_wallpaper(path: str):
+    for (root, dirs, files) in os.walk(path):
+        for f in files:
+            fpath = os.path.join(root, f)
+            wallpapers.append(fpath)
+    subprocess.run(["feh", "--bg-fill", random.choice(wallpapers)])
+
+home = os.getenv("HOME")
+set_random_wallpaper(f"{home}/Pictures/wallpapers/")
--- autostart.sh
+++ autostart.sh
@@ -7,17 +7,6 @@ function run {
     fi
 }
 
-function set_random_wallpaper {
-    for x in ~/Pictures/wallpapers/*; do
-	var=$RANDOM
-	var=$[ 0 + $var % (1 + 1 - 0) ]
-	if [ "$var" = "1" ]; then
-	    feh --bg-fill $x &
-	    break
-	fi
-    done
-}
-
 run dex $HOME/.config/autostart/arcolinux-welcome-app.desktop
 #run xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal
 #run xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
@@ -33,9 +22,7 @@ run numlockx on
 run volumeicon
 #run nitrogen --restore
 #run conky -c $HOME/.config/awesome/system-overview
-picom --config ~/.config/awesome/picom.conf
-
-set_random_wallpaper
+picom --config ~/.config/awesome/picom.conf &
 
 #run applications from startup
 #run firefox

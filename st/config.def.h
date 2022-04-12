eb2fd2de3ce7ab1207319af8f7964d3778ff34ab
robertohermenegildodias <roberto011herminegildo@gmail.com>
2022-04-11 22:56:29 -0300

    Change st's background color

--- st/config.def.h
+++ st/config.def.h
@@ -121,7 +121,7 @@ static const char *colorname[] = {
 	"#cccccc",
 	"#555555",
 	"gray90", /* default foreground colour */
-	"black", /* default background colour */
+	"#181818", /* default background colour */
 };
 
 
